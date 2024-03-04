dfd82e3a7e3f11136417e64b7cada3cedd4f33f3
Kristen Roggero <kroggero@Kristen-Roggeros-MacBook-Air.local>
2024-03-04 12:32:36 -0600

    added styling as well as the components/sections of the grocery list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return(
+    <>
+      <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
+    </>
+  )
+};
+
+function Logo() {
+  return(
+    <h1>What's Cookin'?</h1>
+  )
 }
 
-export default App;
+function Form() {
+  return(
+    <>
+      <div className="add-form">
+        <h3>What ingredients do you need?</h3>
+
+      </div>
+    
+    </>
+  )
+}
+
+function PackingList() {
+  return(
+    <div className="list">
+
+    </div>
+  )
+}
+
+
+function Stats() {
+  return(
+    <footer className="stats">
+      You've got X items on the grocery list, 
+      and you've got X items already (X%)
+    </footer>
+  )
+}
